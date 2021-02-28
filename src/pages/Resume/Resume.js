33850e0cdc389788dfd279c298e9c9848f8209a7
kedareshubham11 <kedareshubham11@gmail.com>
2021-02-28 17:28:02 +0530

    added Resume Timline

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,20 +1,57 @@
 import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import "./Resume.css";
-
+import data from "../../utils/portfolioData";
+import CustomTimeline, { CustomTimelineSeparator } from '../../components/Timeline/CustomTimeline';
+import WorkIcon from '@material-ui/icons/Work';
+import SchoolIcon from '@material-ui/icons/School';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineContent from '@material-ui/lab/TimelineContent';
 function Resume() {
+    
     return (
         <div>
             {/* About Me  */}
-            <Grid container className="section"></Grid>
-                <Grid item className="section__title">
+            <Grid container className="section pb__45">
+                <Grid item className="section__title mb__30">
                     <span></span>
-                    <Typography variant="h6">Aboute Me</Typography>
+                    <h6>About Me</h6>
                 </Grid>
 
+                <Grid item xs={12}>
+                    <Typography variant="body2" className="aboutme__text">{data.about}</Typography>
+                </Grid>
+            </Grid>
             {/* Education and Experiences */}
-            <Grid container className="section"></Grid>
+            <Grid container className="section">
+                <Grid item className="section__title mb__30">
+                    <span></span>
+                    <h6>Resume</h6>
+                </Grid>
 
+                <Grid item xs={12}>
+                    <Grid container className="resume__timeline">
+                        {/* Work */}
+                        <Grid item sm={12} md={6}>
+                            <CustomTimeline title={"Education"} icon={<SchoolIcon /> } >
+                                {data.education.map(education => (
+                                    <TimelineItem>
+                                        <CustomTimelineSeparator />
+                                        <TimelineContent className="timeline__content">
+                                            <Typography className="timeline__title">{education.title}</Typography>
+                                            <Typography className="timeline__date">{education.date}</Typography>
+                                            <Typography variant="body2" className="timeline__description">{education.description}</Typography>
+                                        </TimelineContent>
+                                    </TimelineItem>
+                                ))}
+                            </CustomTimeline>
+
+                        </Grid>
+                        {/* Education */}
+                        <Grid item sm={12} md={6}></Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
             {/* Skills */}
             <Grid container className="section"></Grid>
 
--- src/utils/portfolioData.js
+++ src/utils/portfolioData.js
@@ -2,8 +2,9 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import { WebOutlined } from '@material-ui/icons';
 
-export default {
+let data = {
     name: "Shubham Kedare",
     title: "Full stack Developer",
     dob: "12th May 1998",
@@ -32,5 +33,81 @@ export default {
             icon: <InstagramIcon />,
         }
         
-    }
+    },
+
+    about: "A disciplined and hardworking individual looking forward for a career with an organization which can help me with opportunities to enhance my knowledge, skills and techniques which in turn will be beneficial for the organization. Skilled in Full Stack Web Development, MERN stack, GitHub, Python (Programming Language), and Django. Strong engineering professional with a Bachelor's degree focused in Computer Science from Marathwada Institute of Technology Engineering College, Aurangabad. ",
+    education: [
+        {
+            title: "Bachelor of Engineering",
+            date: "2017 - 2020",
+            description: "Computer Science and Engineering \n MIT College Auranabad."
+        },
+        {
+            title: "Diploma",
+            date: "2014 - 2017",
+            description: "Computer Engineering \n CSMSS COllege of Polytechnic Aurangabad."
+        },
+        {
+            title: "SSC",
+            date: "2013 - 2014",
+            description: ""
+        }
+    ],
+
+    services: [
+        {
+            title: "Web Developer",
+            description: "I am Fullstack Web Developer",
+            icon : <WebOutlined />
+        },
+        {
+            title: "Web Developer",
+            description: "I am Fullstack Web Developer",
+            icon : <WebOutlined />
+        },
+        {
+            title: "Web Developer",
+            description: "I am Fullstack Web Developer",
+            icon : <WebOutlined />
+        }
+    ],
+
+    skilss: [
+        {
+            title: "FRONT-END",
+            description: [
+                "ReactJS",
+                "Javascript",
+                "Boostrap",
+                "Material UI",
+            ],
+        },
+        {
+            title: "BACKE-END",
+            description: [
+                "NodeJs",
+                "Python",
+                "Java"
+            ]
+        },
+        {
+            title: "DATABASES",
+            description: [
+                "Firebase",
+                "MySQL",
+                "MongoDB"
+            ]
+        },
+        {
+            title: "SOURCE CONTROL",
+            description: [
+                "Git",
+                "GitHub",
+                "SCRUM/Agile"
+            ]
+        }
+    ]
+
+
 };
+export default data;
